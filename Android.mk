1892adac9019823f63c4745215f4438f5d123c29
SpiritCroc <dev@spiritcroc.de>
2019-09-05 16:24:42 +0200

    [TMP] AE: Fix Makefile for Android 10
    Disable automatic java code generation for now, can be done manually as
    well or maybe a better way can be found later
    
    Change-Id: Ibb78139a40c8f21ff728f1d8ad065a3993b1b8d1

--- Android.mk
+++ Android.mk
@@ -16,8 +16,8 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 # Auto-generate AeFragmentList for searchable fragments
-$(LOCAL_PATH)/src/com/aicp/extras/search/AeFragmentList.java: $(LOCAL_PATH)/gather_search_fragments.sh $(LOCAL_PATH)/AeFragmentList.java $(foreach dir, res/xml/ src/com/aicp/extras/fragments/, $(wildcard $(LOCAL_PATH)/$(dir)/*))
-	bash $<
+#$(LOCAL_PATH)/src/com/aicp/extras/search/AeFragmentList.java: $(LOCAL_PATH)/gather_search_fragments.sh $(LOCAL_PATH)/AeFragmentList.java $(foreach dir, res/xml/ src/com/aicp/extras/fragments/, $(wildcard $(LOCAL_PATH)/$(dir)/*))
+#	bash $<
 
 LOCAL_MODULE_TAGS := optional
 
@@ -25,7 +25,7 @@ LOCAL_PROGUARD_ENABLED := disabled
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 # Depend on auto-generated source file
-LOCAL_SRC_FILES += src/com/aicp/extras/search/AeFragmentList.java
+#LOCAL_SRC_FILES += src/com/aicp/extras/search/AeFragmentList.java
 
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 
